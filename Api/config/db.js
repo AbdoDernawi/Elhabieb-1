8dea70defad6ad7d4766c9b35141ebebf53d495e
AbdoDernawi <78221433+AbdoDernawi@users.noreply.github.com>
2024-08-21 01:10:20 +0300

    main

--- Api/config/db.js
+++ Api/config/db.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("-ˏˋ⋆ ᴡ ᴇ ʟ ᴄ ᴏ ᴍ ᴇ ⋆ˊˎ- ");
   } catch (err) {
     console.error("Failed to connect to MongoDB", err);
